dc414a22371132f65c957788e9d5c94e83ffdd22
Bradley Gilmore <bradleygilmoreefuf@yahoo.com>
2017-06-28 10:44:31 -0600

    Add Colors file, build out TimerViewController

--- Timer/Timer/TimerViewController.swift
+++ Timer/Timer/TimerViewController.swift
@@ -41,6 +41,9 @@ class TimerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         return String(row)
     }
     
+    //MARK: - Properties
+    
+    var timer = Timer()
     
     //MARK: - IBOutlets
     
@@ -52,6 +55,77 @@ class TimerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var pickerStackView: UIStackView!
     
+    
     //MARK: - IBActions
+    
+    
+    
+    //MARK: Private
+    
+    private func toggleTimer() {
+        if timer.isOn {
+            timer.stop()
+            switchToPickerView()
+        } else {
+            switchToTimerView()
+            
+            let hours = hoursPickerView.selectedRow(inComponent: 0)
+            let minutes = minutesPickerView.selectedRow(inComponent: 0) + (hours * 60)
+            let totalSecondsSetOnTimer = TimeInterval(minutes * 60)
+            
+            timer.setTimer(totalSecondsSetOnTimer, totalSeconds: totalSecondsSetOnTimer)
+            updateTimerBasedViews()
+            timer.start()
+        }
+    }
+    
+    private func updateTimerLabel() {
+        timerLabel.text = timer.timeRemainingString
+    }
+    
+    private func updateProgressView() {
+        
+        let secondsElasped = timer.totalSeconds - timer.secondsRemaining
+        let progress = Float(secondsElasped) / Float(timer.totalSeconds)
+        progressView.setProgress(progress, animated: true)
+    }
+    
+    private dynamic func updateTimerBasedViews(_ notification: Notification) {
+        updateTimerBasedViews()
+    }
+    
+    private func updateTimerBasedViews() {
+        updateTimerLabel()
+        updateProgressView()
+    }
+    
+    private dynamic func timerComplete(_ notification: Notification) {
+        switchToPickerView()
+    }
+    
+    private func switchToTimerView() {
+        timerLabel.isHidden = false
+        progressView.setProgress(0.0, animated: false)
+        progressView.isHidden = false
+        pickerStackView.isHidden = true
+        startButton.setTitle("Cancel", for: [])
+        
+        startButton.setTitleColor(UIColor.blueTimerColor, for: [])
+        startButton.layer.borderColor = UIColor.blueTimerColor.cgColor
+        pauseButton.setTitleColor(UIColor.lightBlueTimerColor, for: [])
+        pauseButton.layer.borderColor = UIColor.lightBlueTimerColor.cgColor
+    }
+    
+    private func switchToPickerView() {
+        pickerStackView.isHidden = false
+        timerLabel.isHidden = true
+        progressView.isHidden = true
+        startButton.setTitle("Start", for: [])
+        
+        startButton.setTitleColor(UIColor.lightBlueTimerColor, for: [])
+        startButton.layer.borderColor = UIColor.lightBlueTimerColor.cgColor
+        pauseButton.setTitleColor(UIColor.blueTimerColor, for: [])
+        pauseButton.layer.borderColor = UIColor.blueTimerColor.cgColor
+    }
     
 }
